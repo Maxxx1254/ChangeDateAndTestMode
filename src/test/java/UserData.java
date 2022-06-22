8486a2a3fa70ef6b1aa2b1e93a1874b7b485c9d4
Maxxx1254 <orenburg20172017@gmail.com>
2022-06-22 14:25:28 +0300

    Новая ветка с тестовым режимом

--- src/test/java/UserData.java
+++ src/test/java/UserData.java
@@ -5,8 +5,7 @@ import lombok.RequiredArgsConstructor;
 @Data
 @RequiredArgsConstructor
 public class UserData {
-    private final String city;
-    private final String date;
-    private final String name;
-    private final String phone;
+    private final String login;
+    private final String password;
+    private final String status;
 }
